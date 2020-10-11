d1b9238927e690d16043ab3a6e6b33b24502a58c
hariharanrl <47934715+hariharanrl@users.noreply.github.com>
2020-10-11 10:23:10 +0530

    Add files via upload
    sample program using Sentence-transformer library

---
+++ sent_similarity.py
@@ -0,0 +1,18 @@
+!pip install -U sentence-transformers
+import torch
+from sentence_transformers import SentenceTransformer,util
+embedder=SentenceTransformer('xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+corpus=['boy','girl','india','fruit']
+corpus_embed = embedder.encode(corpus, convert_to_tensor=True)
+queries=['orange']
+top_k=4
+for query in queries:
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embed)[0]
+    cos_scores = cos_scores.cpu()
+    top_results = torch.topk(cos_scores, k=top_k)
+    print("Query:", query)
+    print("\nTop most similar sentences in corpus:")
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        print(corpus[idx], "(Score: %.4f)" % (score))
